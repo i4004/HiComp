2a02d46d9d2bf3f53c2c5bd83554af50d80fccc9
Alexanderius <alexander.krylkov@gmail.com>
2017-02-03 16:40:14 +0600

    imports parsing, xml doc parsing

--- HiComponent/HiTemplate.cs
+++ HiComponent/HiTemplate.cs
@@ -1,12 +1,31 @@
-﻿using Simplify.Templates;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using Simplify.Templates;
 
 namespace HiComponent
 {
 	public static class HiTemplate
 	{
+		private const string ImportCommandName = "import";
+
 		public static ITemplate Load(this ITemplate tpl)
 		{
 			// Do processing
+
+			var imports = new List<string>();
+			var items = new StringBuilder();
+
+			foreach (var item in tpl.Get().Split(new[] { Environment.NewLine }, StringSplitOptions.None))
+			{
+				if (item.StartsWith(ImportCommandName))
+					imports.Add(item.Substring(item.IndexOf(ImportCommandName, StringComparison.Ordinal)));
+				else
+					items.AppendLine(item);
+			}
+
+			var doc = XDocument.Parse(items.ToString());
 
 			return Template.FromString(tpl.Get());
 		}
